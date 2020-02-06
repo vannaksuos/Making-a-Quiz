9104748191648f2c2e00838004fe99fe268c4326
Vannak <vannaksuos@gmail.com>
2020-02-06 18:18:50 -0500

    pushing unfinished hw

--- javascript.js
+++ javascript.js
@@ -1,44 +1,120 @@
+var startButton = document.getElementById('start-btn')
+var nextButton = document.getElementById('next-btn')
+var questionContainerElement = document.getElementById('question-container')
+var questionElement = document.getElementById('question')
+var choicesElement = document.getElementById('choices-buttons')
 
-var question = [
-    {
-        prompt:"What state do you live in?"\(a)\ California\n\ (b) Florida\n\(c) Texas\n\(d) Washington",)
-        answer: "a"
-    },
-    {
-        prompt: "Which team won the superbowl?"\(a)\ Raiders\n\ (b) Cowboys\n\(c) 49ers\n\(d) Patriots",
-        answer: "c"
-    },
-    {
-        prompt: "What team did Kobe Bryant played for?"\(a)\ Clippers\n\ (b) Lakers\n\(c) Bulls\n\(d) Nuggets",
-        answer: "b"
-    },
+// var countDownTimer;
+//Set the end date for the timer.
+//Make the timer function update every second.
+//Calculate the remaining time in days, hours, minutes, and seconds.
+//Display the output to users.
+//Display a message when the timer is over.
+//Implementation.
 
-    {
-        prompt: "How old do citizens have to be to vote for President?"\(a)\ 16\n\ (b) 21\n\(c) 19\n\(d) 18",
-        answer: "d"
-    },
-];
+console.log(startButton.classList);
 
-var score = 0;
+startButton.addEventListener('click', startGame)
 
-for(var i=0; i <question.length; i++) {
-    var reponse = window.prompt(questions[i].prompt);
-    if(response == questions[i].answer){
-        score++;
-        alert("Corret!");
-    } else{
-        alert ("Wrong!");
-    }
+function startGame() {
+    console.log('Started')
+    //add the hide class and it hides the start button
+    startButton.classList.add('hide')
+    //removing the hide class from the questionContainerElement
+    //the questionContainerElement will now show.
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
-alert("You got " + score + "/" + questions.length);
- //click the start button
-// a timer starts and I am presented with a question
 
-// I answer a question
-// I am presented with another question
-// I answer a question incorrectly
-// time is subtracted from the clock
-// all questions are answered or the timer reaches 0
-// the game is over
-// the game is over
-// I can save my initials and score
+function setNextQuestion(){
+    resetState()
+    showQuestion()
+}
+    
+function showQuestion(){
+  //change the text of the question div from "Question" to whatever Question we have 
+  // questionElement.textContent = 
+  console.log(questions[0])
+  // cahnge the text of our answer buttons with our answers.
+
+
+
+
+
+    // questionContainerElement.innerText = question.question
+  //   question.question.forEach(function(answer) {
+  //     var button = document.createElement.apply('button')
+  //     button.innerText =answer.innerText
+  //     button.classList.add('btn')
+  //   if (answer.correct) {
+  //     button.dataset.correct = answer.correct
+  //   }
+  // button.addEventListener('click', selectAnswer)
+  // choicesButtonsElement.appendChild(button)  
+  // })
+
+}
+
+  
+function resetState() {
+  //hides the nextButton
+  nextButton.classList.add('hide')
+}
+
+// function setStatusClass(element, correct) {
+//   clearStatusClass(element)
+//   if (correct) {
+//     element.classList.add('correct')
+//   } 
+//   else {
+//     element.classList.add('wrong')
+//   }
+// }
+
+// function clearStatusClass(element) {
+//   element.classList.remove("correct")
+//   element.clearStatusClass.remove('wrong')
+// }
+
+// function selectAnswer(e){
+//   var selectButton = e.target
+//   var correct = selectButton.dataset.correct
+//   setStatusClass(document.body, correct)
+//   Array.from(choicesButtonsElement.children).forEach(button => {
+//     setStatusClass(button, button.dataset.correct)
+//   })
+// }
+
+var questions = [
+    {
+      question: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      question: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      question: "Arrays in JavaScript can be used to store ____.",
+      choices: ["numbers and strings",
+        "other arrays",
+        "booleans",
+        "all of the above"
+      ],
+      answer: "all of the above"
+    },
+    {
+      question:
+        "String values must be enclosed within ____ when being assigned to variables.",
+      choices: ["commas", "curly brackets", "quotes", "parentheses"],
+      answer: "quotes"
+    },
+    {
+      question:
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      answer: "console.log"
+    }
+]
